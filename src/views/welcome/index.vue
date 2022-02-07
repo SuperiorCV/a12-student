a61cb514989e0efc02b9fb7a86599da37ed1f019
Langwenchong <1422257646@qq.com>
2022-02-07 21:47:03 +0800

    目录重构

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="welcome">
-    <i class='fad fa-books'></i>
+    <i class="fad fa-books"></i>
+    <el-button></el-button>
   </div>
 </template>
 
 <script>
 export default {
   name: "welcome",
-  created(){
-    this.apis.welcome.initData()
-  }
+  created() {
+    this.apis.welcome.initData();
+  },
 };
 </script>
 
 <style scoped>
-#welcome{
-    width:100%;
-    height:100vh;
-    background:pink
+#welcome {
+  width: 100%;
+  height: 100vh;
+  background: pink;
 }
 </style>
