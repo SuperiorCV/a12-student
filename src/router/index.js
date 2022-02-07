ddb6fc81c39d2a0d2dda60a0c18ea6d0e56099c9
Langwenchong <1422257646@qq.com>
2022-02-07 20:54:49 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'welcome',
+      component: () => import('@/views/welcome/index.vue')
+    },
+    {
+      path: '/welcome',
+      name: 'welcome',
+      compnent: () => import('@/views/welcome/index.vue')
     }
   ]
 })
---
+++ src/views/welcome/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="welcome"></div>
+</template>
+
+<script>
+export default {
+  name: "welcome",
+};
+</script>
+
+<style scoped>
+#welcome{
+    width:100%;
+    height:100vh;
+    background:pink
+}
+</style>
