5fadc7969337fb86036ec642c34c264104c00047
Langwenchong <1422257646@qq.com>
2022-02-10 15:57:14 +0800

    资料卡完成

--- src/components/welcome/account.vue
+++ src/components/welcome/account.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="account">
     <div class="tip">
-      欢迎你加入本教考分离系统，初次账户创建您还需要完善以下个人信息！
+      欢迎加入本教考系统,初次创建账户您还需要完善以下个人信息！
     </div>
     <div class="l-wrapper">
       <el-form
@@ -48,8 +48,43 @@
           <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
       </el-form>
+      <div class="control">
+        <div class="button">提交信息</div>
+        <router-link :to="{ name: 'sign' }" class="link"
+          >已有账号？ 返回登录</router-link
+        >
+      </div>
     </div>
-    <div class="r-wrapper"></div>
+    <div class="r-wrapper">
+      <div class="info">
+        为了方便进行个人人脸识别认证，请您在这里上传个人半身照，如下示例图所示:
+      </div>
+      <div class="ref" @mouseover="upload = true" @mouseout="upload = false">
+        <img
+          v-show="!upload"
+          src="../../../static/images/welcome/reference.png"
+          class="animate__animated animate__fadeIn"
+          height="164px"
+          width="auto"
+        />
+        <div class="upload animate__animated animate__fadeIn" v-show="upload">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </div>
+        <ol>
+          <li>请不要佩戴口罩遮挡面部饰品</li>
+          <li>人像居中且背景图白底最佳</li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -79,11 +114,15 @@ export default {
     var validateSchool = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请输入就读学校"));
+      } else if (value.trim() === "") {
+        callback(new Error("至少要包括一位非空符号"));
       } else {
         callback();
       }
     };
     return {
+      imageUrl: "",
+      upload: false,
       ruleForm: {
         name: "",
         phoneNumber: "",
@@ -111,6 +150,21 @@ export default {
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
   },
 };
 </script>
@@ -126,7 +180,7 @@ export default {
   position: absolute;
   border-radius: 5px;
   box-shadow: 5px 10px 20px 0px rgba(175 187 204 / 30%);
-  padding: 100px 40px 40px 40px;
+  padding: 40px;
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
@@ -138,25 +192,96 @@ export default {
   font-size: 1.2rem;
   font-weight: 500;
   font-family: SentyGoldenBell;
-  color: #ff6b6b;
+  color: #636e72;
 }
 .l-wrapper {
   width: 65%;
   height: 100%;
   /* background: green; */
   box-sizing: border-box;
-  padding: 0 40px 0 0;
+  padding: 60px 40px 0 0;
   display: flex;
-  justify-content: stretch;
+  /* justify-content: stretch; */
   align-items: center;
+  flex-direction: column;
 }
 .ruleForm {
   width: 100%;
+}
+.control {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.button {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  box-sizing: border-box;
+  width: 360px;
+  height: 48px;
+  background: url("../../../static/images/welcome/button-bg.png") no-repeat 0 0;
+  background-size: cover;
+  box-shadow: 0 3px 12px 0 rgb(39 125 255 / 30%);
+  border-radius: 24px;
+  font-size: 18px;
+  color: #ffffff;
+  outline: none;
+  cursor: pointer;
+  border: none;
+}
+.button:hover {
+  background-position: 0 -48px;
+  background-size: cover;
+}
+.link {
+  color: #40485b;
+  transition: 0.5s;
+  font-size: 14px;
+  text-decoration: none;
+}
+.link:hover {
+  color: #ff7675;
 }
 .r-wrapper {
   width: 35%;
   height: 100%;
   box-sizing: border-box;
   padding: 0 0 0 40px;
+}
+.info {
+  margin-top: 30px;
+  margin-bottom: 20px;
+  box-sizing: border-box;
+  padding-left: 20px;
+  position: relative;
+}
+.info:before {
+  content: "";
+  height: 75px;
+  width: 110px;
+  top: -40px;
+  left: -70px;
+  position: absolute;
+  transform: scale(0.5);
+  background-image: url("../../../static/images/welcome/icon_quote.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.ref {
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 90%;
+  padding: 20px 10px;
+  border: 1px #333 dashed;
+}
+ol {
+  margin: 0;
+  margin-top: 20px;
 }
 </style>
