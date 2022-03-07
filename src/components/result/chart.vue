87ec35ba16dc0292a8efcc8e2470a0892bb8ffc3
Langwenchong <1422257646@qq.com>
2022-03-07 11:09:55 +0800

    试卷结果分析

---
+++ src/components/result/chart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="chart">
+    <div class="header">
+      <h3>考试结果分析</h3>
+    </div>
+    <div class="main"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chart",
+};
+</script>
+
+<style scoped>
+#chart {
+  width: 100%;
+  height: 500px;
+  box-sizing: border-box;
+}
+.header {
+  display: flex;
+  align-items: center;
+  height: 64px;
+  box-sizing: border-box;
+  padding: 10px;
+  background: #fafafa;
+  border-top: 1px solid #dedede;
+  border-bottom: 1px solid #dedede;
+}
+.main {
+  height: 100%;
+  width: 100%;
+  background: pink;
+}
+</style>
